442c5284dfd96ffacd04ca74a683d1dfdddac7e0
earowang <earo.wang@gmail.com>
2016-12-16 15:44:29 +1100

    added pedestrian to the data folder and documented

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#' Pedestrian counts in Melbourne city
+#'
+#' A dataset containing the pedestrian counts at hourly intervals over the year
+#' of 2015 at 8 sensors in the city of Melbourne. The variables are as follows:
+#'
+#' @format A data frame with 70072 rows and 4 variables:
+#' \describe{
+#'   \item{Date_Time}{Date time in 2015}
+#'   \item{Sensor_ID}{Sensor identifiers}
+#'   \item{Sensor_Name}{Sensor names}
+#'   \item{Hourly_Counts}{Pedestrian Counts at hourly intervals}
+#' }
+#' @docType data
+#' @name pedestrian
+#' @usage pedestrian
+#' @examples
+#' pedestrian
+NULL
