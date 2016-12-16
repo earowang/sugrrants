8f51a91ccd7d58b2e1fa98d138196caed67daf25
earowang <earo.wang@gmail.com>
2016-12-16 12:43:19 +1100

    added on_period.R

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,10 @@
-#' @importFrom lubridate as_date wday days month weeks
+#' @importFrom lubridate as_date wday days month weeks ymd year
 #' @importFrom timeDate GoodFriday Easter
-#' @importFrom dplyr if_else
+#' @importFrom dplyr if_else case_when mutate mutate_ group_by group_by_
+#' @importFrom dplyr is.grouped_df groups ungroup select filter
 #' @importFrom tibble tibble
+#' @importFrom tidyr unnest spread_
+#' @importFrom purrr by_slice
+#' @importFrom lazyeval f_interp f_eval_rhs f_capture
+#' @importFrom magrittr %>%
 NULL
