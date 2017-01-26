60be183fe489c1a10021e6cab9760e16b9d14e8a
earowang <earo.wang@gmail.com>
2017-01-26 16:04:43 +1100

    remove season-like functions and done with the as_period.rd

--- R/as_period.R
+++ R/as_period.R
@@ -1,126 +1,89 @@
-#' Get seasons component of a date-time
-#'
-#' @param x A date-time object.
-#' @param hemisphere A character vector that specifies the hemisphere. "N" refers
-#'    to the Northern hemisphere, and "S" to the Souther hemisphere.
-#' @param definition A character vector that defines the season range. "meterological"
-#'    is the only one supported at the stage.
-#'
-#' @return The season of \code{x} as character string.
-#'
-#' @seealso as_season
-#'
-#' @author Earo Wang
-#' @references 
-#'    Barnett, A. G. and Dobson A. J. (2010) Analysing seasonal health data. Springer.
-#' 
-#' @export
-#'
-#' @examples
-#'    x <- as.POSIXct(c("2014-01-31", "2015-07-31", "2016-10-31"))
-#'    season(x, hemisphere = "S") # I'm in Australia
-#'
-season <- function(x, hemisphere = c("N", "S"), 
-                   definition = c("meterological", "astronomical", 
-                                  "midsolstice")) {
-  hemisphere <- match.arg(hemisphere)
-  definition <- match.arg(definition)
-  month_x <- month(x)
-  if (hemisphere == "N") {
-    season_x <- case_when(
-      month_x %in% 3:5 ~ "Spring",
-      month_x %in% 6:8 ~ "Summer",
-      month_x %in% 9:11 ~ "Autumn",
-      month_x %in% c(12, 1:2) ~ "Winter"
-    )
-  } else {
-    season_x <- case_when(
-      month_x %in% 3:5 ~ "Autumn",
-      month_x %in% 6:8 ~ "Winter",
-      month_x %in% 9:11 ~ "Spring",
-      month_x %in% c(12, 1:2) ~ "Summer"
-    )
-  }
-  return(season_x)
-}
-
-#' @rdname as_month
-#'
-#' @param hemisphere A character vector that specifies the hemisphere. "N" refers
-#'    to the Northern hemisphere, and "S" to the Souther hemisphere.
-#' @param definition A character vector that defines the season range. "meterological"
-#'    is the only one supported at the stage.
-#'
-#' @references 
-#'    Barnett, A. G. and Dobson A. J. (2010) Analysing seasonal health data. Springer.
-#'
-#' @export
-as_season <- function(x, hemisphere = "N", definition = "meterological",
-                      date_format = TRUE) {
-  hemisphere <- match.arg(hemisphere, c("N", "S"))
-  definition <- match.arg(
-    definition, c("meterological", "astronomical", "midsolstice")
-  )
-
-  year_x <- year(x)
-  season_x <- season(x, hemisphere = hemisphere, definition = definition)
-  if (date_format) {
-    if (hemisphere == "N") {
-      season_date <- case_when(
-        season_x == "Spring" ~ "03-01",
-        season_x == "Summer" ~ "06-01",
-        season_x == "Autumn" ~ "09-01",
-        season_x == "Winter" ~ "12-01"
-      )
-    } else {
-      season_date <- case_when(
-        season_x == "Autumn" ~ "03-01",
-        season_x == "Winter" ~ "06-01",
-        season_x == "Spring" ~ "09-01",
-        season_x == "Summer" ~ "12-01"
-      )
-    }
-    output <- ymd(paste(year_x, season_date, sep = "-"))
-  } else {
-    output <- paste(year_x, season_x, sep = "-")
-  }
-  return(output)
-}
+# season <- function(x, hemisphere = c("N", "S"), 
+#                    definition = c("meterological", "astronomical", 
+#                                   "midsolstice")) {
+#   hemisphere <- match.arg(hemisphere)
+#   definition <- match.arg(definition)
+#   month_x <- month(x)
+#   if (hemisphere == "N") {
+#     season_x <- case_when(
+#       month_x %in% 3:5 ~ "Spring",
+#       month_x %in% 6:8 ~ "Summer",
+#       month_x %in% 9:11 ~ "Autumn",
+#       month_x %in% c(12, 1:2) ~ "Winter"
+#     )
+#   } else {
+#     season_x <- case_when(
+#       month_x %in% 3:5 ~ "Autumn",
+#       month_x %in% 6:8 ~ "Winter",
+#       month_x %in% 9:11 ~ "Spring",
+#       month_x %in% c(12, 1:2) ~ "Summer"
+#     )
+#   }
+#   return(season_x)
+# }
+#
+# as_season <- function(x, hemisphere = "N", definition = "meterological",
+#                       date_format = TRUE) {
+#   hemisphere <- match.arg(hemisphere, c("N", "S"))
+#   definition <- match.arg(
+#     definition, c("meterological", "astronomical", "midsolstice")
+#   )
+#
+#   year_x <- year(x)
+#   season_x <- season(x, hemisphere = hemisphere, definition = definition)
+#   if (date_format) {
+#     if (hemisphere == "N") {
+#       season_date <- case_when(
+#         season_x == "Spring" ~ "03-01",
+#         season_x == "Summer" ~ "06-01",
+#         season_x == "Autumn" ~ "09-01",
+#         season_x == "Winter" ~ "12-01"
+#       )
+#     } else {
+#       season_date <- case_when(
+#         season_x == "Autumn" ~ "03-01",
+#         season_x == "Winter" ~ "06-01",
+#         season_x == "Spring" ~ "09-01",
+#         season_x == "Summer" ~ "12-01"
+#       )
+#     }
+#     output <- ymd(paste(year_x, season_date, sep = "-"))
+#   } else {
+#     output <- paste(year_x, season_x, sep = "-")
+#   }
+#   return(output)
+# }
 
 #' @name as_month
-#' @aliases as_season
 #' @aliases as_week
 #'
-#' @title Extract the calendar period components from a date-time
+#' @title Extract the calendar period components from date-times
 #'
-#' @description Extract the calendar period components from a date-time
+#' @description Extract the higher-level calendar period components from date-times
 #'
-#' @param x A date-time object.
-#' @param date_format Logical. If TRUE, it returns a date format object; otherwise
-#'    a character string.
+#' @param x Date-times.
 #'
-#' @return The period of \code{x} as date format or character string.
+#' @return The period of \code{x} as date format or character string. It always
+#'    returns the first day of the month (\code{as_month}) and the first day of
+#'    the week (\code{as_week}).
 #'
 #' @author Earo Wang
 #' 
 #' @rdname as_month
+#' 
 #' @export
 #'
 #' @examples
 #'    x <- as.POSIXct(c("2014-01-31", "2015-07-31", "2016-10-31"))
 #'    as_month(x)
+#'    as_week(x)
 #'
 # apply to monthly data
 # x takes datetime stamp and return the year-month-first of day
-as_month <- function(x, date_format = TRUE) {
+as_month <- function(x) {
   year_x <- year(x)
-  if (date_format) {
-    month_x <- formatC(month(x), width = 2, flag = "0")
-    output <- ymd(paste0(year_x, month_x, "01"))
-  } else {
-    month_x <- month(x, label = TRUE, abbr = TRUE)
-    output <- paste(year_x, month_x, "-")
-  }
+  month_x <- formatC(month(x), width = 2, flag = "0")
+  output <- ymd(paste0(year_x, month_x, "01"))
   return(output)
 }
 
@@ -128,27 +91,21 @@ as_month <- function(x, date_format = TRUE) {
 #' @export
 # apply to weekly data
 # x takes datetime stamp and return the first date of the week
-as_week <- function(x, date_format = TRUE) {
+as_week <- function(x) {
   wday_x <- wday(x, label = FALSE, abbr = FALSE)
-  if (date_format) {
-    diff_wday <- if_else(wday_x == 1, -6, 2 - wday_x) # find first date in the week
-    output <- as_date(x + days(diff_wday))
-  } else {
-    chr_x <- paste0("W", formatC(wday_x, width = 2, flag = 0))
-    year_x <- year(x)
-    output <- paste(year_x, chr_x, sep = "-")
-  }
+  diff_wday <- if_else(wday_x == 1, -6, 2 - wday_x) # find first date in the week
+  output <- as_date(x + days(diff_wday))
   return(output)
 }
 
-#' @title Get weekdays component of a date-time
+#' @title Get weekdays component of datetimea
 #'
-#' @description A wrapper of \code{lubridate::wday()}, but allows to define the
-#'    starting weekday.
+#' @description A simple wrapper of \code{lubridate::wday()}, but starts with
+#'    Monday instead of Sunday.
 #'
-#' @param x A date-time object.
+#' @param x Date-times.
 #' @param label Logical. If TRUE, it displays the day of the week as an ordered
-#'    character strings; otherwise, an integer returns
+#'    character strings; otherwise, an integer returns.
 #' @param abbr Logical. If TRUE, it returns an abbreviated version the label.
 #'
 #' @return The day of the week as a number (Monday is 1) or an ordered factor
--- R/by_period.R
+++ R/by_period.R
@@ -1,61 +1,52 @@
 # SE
-by_season_ <- function(.data, .index, .value, .f, ..., .label = ".out",
-                       hemisphere = "N", definition = "meterological",
-                       date_format = TRUE) {
-  .f <- match.fun(uq(.f))
-  hemisphere <- match.arg(hemisphere, c("N", "S"))
-  definition <- match.arg(
-    definition, c("meterological", "astronomical", "midsolstice")
-  )
-  add <- if_else(is.grouped_df(.data), TRUE, FALSE)
-  # fun_name <- expr_text(.f)
+# by_season_ <- function(.data, .index, .value, .f, ..., .label = ".out",
+#                        hemisphere = "N", definition = "meterological",
+#                        date_format = TRUE) {
+#   .f <- match.fun(uq(.f))
+#   hemisphere <- match.arg(hemisphere, c("N", "S"))
+#   definition <- match.arg(
+#     definition, c("meterological", "astronomical", "midsolstice")
+#   )
+#   add <- if_else(is.grouped_df(.data), TRUE, FALSE)
+#   # fun_name <- expr_text(.f)
 
-  .data <- .data %>% 
-    mutate_(.season = f_interp(
-      ~ as_season(uq(.index), hemisphere = hemisphere, definition = definition,
-                  date_format = date_format))) %>% 
-    group_by_(~ .season, add = add)
+#   .data <- .data %>% 
+#     mutate_(.season = f_interp(
+#       ~ as_season(uq(.index), hemisphere = hemisphere, definition = definition,
+#                   date_format = date_format))) %>% 
+#     group_by_(~ .season, add = add)
 
-  # retrieve the grouping variables for later
-  grouped_vars <- groups(.data)
+#   # retrieve the grouping variables for later
+#   grouped_vars <- groups(.data)
 
-  # by_slice should retain the grouping structure
-  .data <- .data %>% 
-    by_slice(~ .f(f_eval_rhs(.value, .x), ...), .collate = "list", .to = .label) 
+#   # by_slice should retain the grouping structure
+#   .data <- .data %>% 
+#     by_slice(~ .f(f_eval_rhs(.value, .x), ...), .collate = "list", .to = .label) 
 
-  .data <- .data %>% 
-    unnest() %>% 
-    group_by_(.dots = grouped_vars)
+#   .data <- .data %>% 
+#     unnest() %>% 
+#     group_by_(.dots = grouped_vars)
 
-  return(.data)
-}
+#   return(.data)
+# }
 
-#' @rdname by_month
-#'
-#' @param hemisphere A character vector that specifies the hemisphere. "N" refers
-#'    to the Northern hemisphere, and "S" to the Souther hemisphere.
-#' @param definition A character vector that defines the season range. "meterological"
-#'    is the only one supported at the stage.
-#'
-#' @export
 # NSE
-by_season <- function(.data, .index, .value, .f, ..., .label = ".out",
-                      hemisphere = "N", definition = "meterological",
-                      date_format = TRUE) {
-  by_season_(.data, f_capture(.index), f_capture(.value), .f, ..., 
-             .label = .label, hemisphere = hemisphere, definition = definition,
-             date_format = date_format)
-}
+# by_season <- function(.data, .index, .value, .f, ..., .label = ".out",
+#                       hemisphere = "N", definition = "meterological",
+#                       date_format = TRUE) {
+#   by_season_(.data, f_capture(.index), f_capture(.value), .f, ..., 
+#              .label = .label, hemisphere = hemisphere, definition = definition,
+#              date_format = date_format)
+# }
 
 # SE
-by_month_ <- function(.data, .index, .value, .f, ..., .label = ".out",
-                      date_format = TRUE) {
+by_month_ <- function(.data, .index, .value, .f, ..., .label = ".out") {
   .f <- match.fun(uq(.f))
   add <- if_else(is.grouped_df(.data), TRUE, FALSE)
   # fun_name <- expr_text(.f)
 
   .data <- .data %>% 
-    mutate_(.month = f_interp(~ as_month(uq(.index), date_format = date_format))) %>% 
+    mutate_(.month = f_interp(~ as_month(uq(.index)))) %>% 
     group_by_(~ .month, add = add)
 
   # retrieve the grouping variables for later
@@ -73,7 +64,6 @@ by_month_ <- function(.data, .index, .value, .f, ..., .label = ".out",
 }
 
 #' @name by_month
-#' @aliases by_season
 #' @aliases by_week
 #' @aliases by_day
 #'
@@ -87,8 +77,6 @@ by_month_ <- function(.data, .index, .value, .f, ..., .label = ".out",
 #' @param .f A function applied to aggregation over the fixed calendar periods.
 #' @param ... The extra arguments passed to \code{.f}.
 #' @param .label a character string to be labelled for the aggregated values.
-#' @param date_format Logical. If TRUE, it returns a date format; otherwise a
-#'    character string.
 #'
 #' @return A data frame that contains the calendar periods and the aggregated values.
 #'
@@ -104,24 +92,22 @@ by_month_ <- function(.data, .index, .value, .f, ..., .label = ".out",
 #' @export
 #'
 # NSE
-by_month <- function(.data, .index, .value, .f, ..., .label = ".out",
-                     date_format = TRUE) {
+by_month <- function(.data, .index, .value, .f, ..., .label = ".out") {
   by_month_(.data, f_capture(.index), f_capture(.value), .f, ..., 
-           .label = .label, date_format = date_format)
+           .label = .label)
 }
 
 # by_week is a window function that applying a function to a fixed window of 
 # a week.
 # If .f returns only one value, it's better to use as_week and summarise
 # SE
-by_week_ <- function(.data, .index, .value, .f, ..., .label = ".out",
-                     date_format = TRUE) {
+by_week_ <- function(.data, .index, .value, .f, ..., .label = ".out") {
   .f <- match.fun(uq(.f))
   add <- if_else(is.grouped_df(.data), TRUE, FALSE)
   # fun_name <- expr_text(.f)
 
   .data <- .data %>% 
-    mutate_(.week = f_interp(~ as_week(uq(.index), date_format = date_format))) %>% 
+    mutate_(.week = f_interp(~ as_week(uq(.index)))) %>% 
     group_by_(~ .week, add = add)
 
   # retrieve the grouping variables for later
@@ -161,10 +147,9 @@ by_week_ <- function(.data, .index, .value, .f, ..., .label = ".out",
 #' @rdname by_month
 #' @export
 # NSE
-by_week <- function(.data, .index, .value, .f, ..., .label = ".out",
-                    date_format = TRUE) {
+by_week <- function(.data, .index, .value, .f, ..., .label = ".out") {
   by_week_(.data, f_capture(.index), f_capture(.value), .f, ..., 
-           .label = .label, date_format = date_format)
+           .label = .label)
 }
 
 # apply to daily data
