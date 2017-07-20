bfd7b6a13e70e480218446f5a543e3436f21e225
earowang <earo.wang@gmail.com>
2017-07-20 19:47:24 +1000

    clean up a bit

--- R/frame-calendar.R
+++ R/frame-calendar.R
@@ -1,3 +1,11 @@
+globalVariables(c(
+  ".group_id", ".gx", ".gy", ".xmajor_min", ".xminor_max", ".xminor_min", 
+  ".ymajor_max", ".ymajor_min", ".yminor_max", ".yminor_min", "COL", "PANEL",
+  "MCOL", "MPANEL", "MROW", "ROW", "date_eval", "label", "margins", "radius", 
+  "theta", "x", "y", ".day", ".calendar_tbl", ".x", ".y", ".", ".ymax", ".ymin",
+  ".cx", ".cy"
+))
+
 #' @title Rearrange a temporal data frame to a calendar-based data format using 
 #'    linear algebra
 #'
