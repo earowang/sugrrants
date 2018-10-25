021dc47e4088397f35a6d70da9c4c3de2db10ab3
earowang <earo.wang@gmail.com>
2018-10-25 16:04:22 +1100

    added `dir` and `scales`

--- R/facet-calendar.R
+++ R/facet-calendar.R
@@ -17,13 +17,22 @@ globalVariables("facet_wrap")
 #'   geom_line() +
 #'   facet_calendar(date = Date, nrow = 2)
 facet_calendar <- function(date, nrow = NULL, ncol = NULL, calendar.label = "mday",
-  scales = "fixed", shrink = TRUE, strip.position = "top") {
+  scales = "fixed", shrink = TRUE, dir = "h", strip.position = "top") {
   calendar.label <- match.arg(calendar.label, c("wday", "mday"))
+
+  scales <- match.arg(scales, c("fixed", "free_x", "free_y", "free"))
+  dir <- match.arg(dir, c("h", "v"))
+  free <- list(
+    x = any(scales %in% c("free_x", "free")),
+    y = any(scales %in% c("free_y", "free"))
+  )
 
   if (calendar.label == "mday") {
     facet <- ggplot2::facet_wrap(~ .month + .mday, nrow = nrow, ncol = ncol,
       scales = scales, shrink = shrink, strip.position = strip.position)
     facet$params$date <- enexpr(date)
+    facet$params$free <- free
+    facet$params$dir <- dir
     ggproto(NULL, FacetCalendarMday,
       shrink = shrink,
       params = facet$params
@@ -40,23 +49,23 @@ FacetCalendarMday <- ggproto("FacetCalendarMday", FacetWrap,
     eval_date <- eval_tidy(params$date, data = data[[1]])
     date_chr <- as_string(params$date)
 
-    layout <- setup_calendar.monthly(eval_date, 
+    layout <- setup_calendar.monthly(eval_date, dir = params$dir,
       nrow = params$nrow, ncol = params$ncol)
+    n <- NROW(layout)
 
     layout %>%
       dplyr::mutate(
         !! date_chr := PANEL,
         .month = lubridate::month(PANEL, label = TRUE),
         .mday = lubridate::mday(PANEL),
-        PANEL = dplyr::row_number(),
-        SCALE_X = 1L,
-        SCALE_Y = 1L
+        PANEL = seq_len(n),
+        SCALE_X = ifelse(params$free$x, seq_len(n), 1L),
+        SCALE_Y = ifelse(params$free$y, seq_len(n), 1L)
       )
   },
 
   map_data = function(data, layout, params) {
     date_chr <- as_string(params$date)
-    data %>%
-      dplyr::left_join(layout, by = date_chr)
+    dplyr::left_join(data, layout, by = date_chr)
   }
 )
