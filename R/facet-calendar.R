08fa651782ae4639a07b3495b7d606cfeef26ffb
earowang <earo.wang@gmail.com>
2018-10-25 23:35:18 +1100

    added space to month panels

--- R/facet-calendar.R
+++ R/facet-calendar.R
@@ -4,8 +4,9 @@ globalVariables("facet_wrap")
 #'
 #' @param date A variable that contains "Date" class.
 #' @param format A character string. See `?strptime` for details.
-#' @param week_start Day on which week starts following ISO conventions - 
-#' 1 means Monday, 7 means Sunday (default).
+#' @param week_start Day on which week starts following ISO conventions -
+#' 1 means Monday, 7 means Sunday (default). You can set `lubridate.week.start` 
+#' option to control this parameter globally.
 #' @inheritParams ggplot2::facet_wrap
 #'
 #' @rdname facet-calendar
@@ -13,14 +14,14 @@ globalVariables("facet_wrap")
 #' @examples
 #' fs <- pedestrian %>%
 #'   dplyr::filter(Sensor_Name == "Flagstaff Station", Date < as.Date("2016-05-01"))
-#' 
+#'
 #' fs %>%
 #'   ggplot(aes(x = Time, y = Hourly_Counts, group = Date)) +
 #'   geom_line() +
 #'   facet_calendar(date = Date, nrow = 2)
-facet_calendar <- function(date, nrow = NULL, ncol = NULL, format = "%b %d", 
-  week_start = 1, scales = "fixed", shrink = TRUE, dir = "h", 
-  strip.position = "top") {
+facet_calendar <- function(date, nrow = NULL, ncol = NULL, format = "%b %d",
+  week_start = getOption("lubridate.week.start", 1), scales = "fixed", 
+  shrink = TRUE, dir = "h", strip.position = "top") {
 
   scales <- match.arg(scales, c("fixed", "free_x", "free_y", "free"))
   dir <- match.arg(dir, c("h", "v"))
@@ -29,14 +30,14 @@ facet_calendar <- function(date, nrow = NULL, ncol = NULL, format = "%b %d",
     y = any(scales %in% c("free_y", "free"))
   )
 
-  facet <- ggplot2::facet_wrap(~ .label, nrow = nrow, ncol = ncol,
-    scales = scales, shrink = shrink, strip.position = strip.position)
+  facet <- facet_wrap(~ .label, nrow = nrow, ncol = ncol, scales = scales, 
+    shrink = shrink, strip.position = strip.position)
   facet$params$date <- enexpr(date)
   facet$params$format <- format
   facet$params$week_start <- week_start
   facet$params$free <- free
   facet$params$dir <- dir
-  ggproto(NULL, FacetCalendarMday,
+  ggproto(NULL, FacetCalendar,
     shrink = shrink,
     params = facet$params
   )
@@ -46,7 +47,7 @@ facet_calendar <- function(date, nrow = NULL, ncol = NULL, format = "%b %d",
 #' @format NULL
 #' @usage NULL
 #' @export
-FacetCalendarMday <- ggproto("FacetCalendarMday", FacetWrap,
+FacetCalendar <- ggproto("FacetCalendar", FacetWrap,
   compute_layout = function(data, params) {
     eval_date <- eval_tidy(params$date, data = data[[1]])
     date_chr <- as_string(params$date)
@@ -68,5 +69,28 @@ FacetCalendarMday <- ggproto("FacetCalendarMday", FacetWrap,
   map_data = function(data, layout, params) {
     date_chr <- as_string(params$date)
     dplyr::left_join(data, layout, by = date_chr)
+  },
+
+  draw_panels = function(self, panels, layout, x_scales, y_scales, ranges, 
+    coord, data, theme, params) {
+
+    canvas <- ggproto_parent(FacetWrap, self)$draw_panels(panels, layout, 
+      x_scales, y_scales, ranges, coord, data, theme, params)
+
+    space_x <- theme$panel.spacing.x %||% theme$panel.spacing
+    row_spacer <- unit(as.double(space_x) * 2, attr(space_x, "unit"))
+    space_y <- theme$panel.spacing.y %||% theme$panel.spacing
+    col_spacer <- unit(as.double(space_y) * 2, attr(space_y, "unit"))
+
+    # No idea why 28 and 26 are position in gtable (perhaps magic numbers)
+    ncol <- max(layout$MCOL)
+    nrow <- max(layout$MROW)
+    for (i in seq(28, by = 28, length.out = ncol - 1)) {
+      canvas <- gtable::gtable_add_cols(canvas, width = col_spacer, pos = i)
+    }
+    for (j in seq(26, by = 26, length.out = nrow - 1)) {
+      canvas <- gtable::gtable_add_rows(canvas, heights = row_spacer, pos = j)
+    }
+    canvas
   }
 )
